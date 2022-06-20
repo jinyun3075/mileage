eeb03bef6e737fb5c693fcd8f7bfb3636b0d0907
jinyunjae <jinyun3075@naver.com>
2022-06-20 20:08:25 +0900

    init

---
+++ src/main/java/com/Triple_HomeWork/Triple_HomeWork/TripleHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.Triple_HomeWork.Triple_HomeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleHomeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleHomeWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Triple_HomeWork/Triple_HomeWork/TripleHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Triple_HomeWork.Triple_HomeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleHomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
